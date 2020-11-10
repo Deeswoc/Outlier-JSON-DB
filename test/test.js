dc5121da52345fc7b2d45fe482c4db2a6bd59a4c
Deeswoc <47671781+Deeswoc@users.noreply.github.com>
2020-11-10 09:29:45 -0500

    removed unnessasary files from rep

--- test/test.js
+++ test/test.js
@@ -3,33 +3,65 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../app');
 let should = chai.should();
-let expect = chai.expect; 
+let expect = chai.expect;
 let fs = require('fs');
 let path = require('path');
 chai.use(chaiHttp);
 let port = 3000;
 let baseUrl = `http://localhost:${port}/`
-describe('Create Student Property', ()=>{
-    let id = "876";
-    describe('Student File does not exists', ()=>{
-        it('should not have a file for the student', ()=>{
-            expect(fs.existsSync(path.join(__dirname, "data", `${id}.json`))).to.be.false;
-        })
 
-        it('should return the name of the student', ()=>{
-            chai.request(server)
-                .post(`/${id}/name`)
-                .set('content-type', 'application/json')
-                .send({ name:"Henry"})
-                .end((err, res)=>{
-                    res.should.have.status(200);
-                    expect(res.body).to.be.eql({ name:"Henry"});
-                })
-        });
-
-        it('should create a file with the given id', ()=>{
-          path.join(__dirname, "data", `${id}.json`)
-        })
-
+let testIDs = [];
+describe('Create Student Property', () => {
+  let id = "876";
+  describe('Student file does not exist', () => {
+    it('should not have a file for the student', () => {
+      expect(fs.existsSync(path.join(__dirname, "../", "data", `${id}.json`))).to.be.false;
     })
+
+    it('should return the name of the student', () => {
+      chai.request(server)
+        .post(`/${id}/name`)
+        .set('content-type', 'application/json')
+        .send({ name: "Henry" })
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body).to.be.eql({ name: "Henry" });
+        })
+    });
+
+    it('should create a file with the given id', () => {
+      expect(fs.existsSync(path.join(__dirname, "../", "data", `${id}.json`))).to.be.true;
+      testIDs.push(id);
+    })
+  })
+
+
+  describe('Student File does Exist', () => {
+    it('should return the name of the student', () => {
+      chai.request(server)
+        .post(`/${id}/Class/Teacher/name`)
+        .set('content-type', 'application/json')
+        .send({ name: "Bill" })
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body).to.be.eql({ name: "Bill" });
+        })
+    });
+
+    it('should still have the students name', () => {
+      let data = fs.readFileSync(path.join(__dirname, "../", "data", `${id}.json`));
+      let student = JSON.parse(data);
+      expect(student.name).to.be.equal("Henry");
+    });
+
+
+  })
+
+})
+
+afterEach(()=>{
+  testIDs.forEach(element => {
+    console.log("Deleting " + element + ".json");
+    fs.unlinkSync(path.join(__dirname, "../", "data", `${element}.json`));
+  })
 })
